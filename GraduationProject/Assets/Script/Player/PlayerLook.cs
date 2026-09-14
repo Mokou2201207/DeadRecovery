0cc0bc73a2e25893617d62dc840d29ee60c6c0e6
Mokou2201207 <24051905@d.anabuki-college.net>
2026-09-15 00:24:10 +0900

    アセット追加、マップ追加
    トラックアセットを追加。Stageのマップを実際に移動可能やロビーからステージへなどに可能

--- GraduationProject/Assets/Script/Player/PlayerLook.cs
+++ GraduationProject/Assets/Script/Player/PlayerLook.cs
@@ -16,11 +16,35 @@ public class PlayerLook : MonoBehaviour
     [Header("クロスヘアの下のText")]
     [SerializeField] private Text crosshairText;
 
+    void Start()
+    {
+        //オブジェクト名で自動アタッチ
+        if (crosshairText == null)
+        {
+            GameObject textObj = GameObject.Find("CrosshairText");
+
+            if (textObj != null)
+            {
+                crosshairText = textObj.GetComponent<Text>();
+            }
+
+            if (crosshairText == null)
+            {
+                Debug.LogWarning("クロスヘア用のテキストが見つかりませんでした！");
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         //カメラが設定されてなかったら何も処理しない
         if(playerCamera==null) return;
+
+        if (crosshairText != null)
+        {
+            crosshairText.text = "";
+        }
 
         //カメラの向きを基準でRayをはる
         Ray ray=playerCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2,0));
@@ -45,10 +69,6 @@ public class PlayerLook : MonoBehaviour
                         teleport.StageTeleport();
                     }
                 }
-            }
-            else
-            {
-                crosshairText.text = "";
             }
         }
 
